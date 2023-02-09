9381c61c105be92679f9048d160709b1433153f1
williamgflores <william.flores4@upr.edu>
2023-02-09 18:39:28 -0400

    Passwords

---
+++ account.py
@@ -0,0 +1,6 @@
+import functions
+
+place = input('Name of the place: ')
+username = input('Username: ')
+password = input('Password: ')
+functions.acount(place, username, password)
---
+++ functions.py
@@ -0,0 +1,17 @@
+def acount (website1, username, password):
+    v = open("acounts", 'a')
+    v.write(website1)
+    v.write('\n')
+    v.write(username)
+    v.write('\n')
+    v.write(password)
+    v.write('\n')
+    
+
+
+
+    
+
+
+    
+    
